44fa67af079d63a3b1fa94c4de7e34c04cb8253d
Flare200 <firebird567@cox.net>
2020-02-13 13:22:43 -0800

    Added functionality to take in a text input and add an li, as well as make sure the dynamically added items could be crossed off and deleted

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,13 +1,24 @@
 //Check off todos by clicking
-$("li").click(function() { 
+$("ul").on("click","li",function() { 
    $(this).toggleClass("completed")
 });
 
 //Click on X to delete todo
-$("span").click(function(e) { 
+$("ul").on("click","span",function(e) { 
     $(this).parent().fadeOut(500,function()
     {
         $(this).remove()
     })
     e.stopPropagation() 
+});
+
+$("input[type='text']").keypress(function(e) { 
+    if(e.which===13)
+    {
+        //Grab text from input
+        var todoText=$(this).val()
+        $(this).val("")
+        //Create new li and add to ul
+        $("ul").append("<li><span>X</span> "+todoText+"</li>")
+    }
 });
